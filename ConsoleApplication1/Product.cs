e6d081f832905a425b28cc82233201b93bb3a88a
thinhnw <thinhnv0412@gmail.com>
2021-05-17 11:32:38 +0700

    Initial

---
+++ ConsoleApplication1/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.CodeDom;
+using System.Runtime.ConstrainedExecution;
+
+namespace ConsoleApplication1
+{
+    public class Product
+    {
+        public const int GALLERY_LENGTH_MAX = 10;
+        // id, name, price, qty, image, desc, gallery < string array>;
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Price { get; set; }
+        private int Qty { get; set; }
+        private string Image { get; set; }
+        private string Desc { get; set; }
+        private string[] Gallery { get; }
+
+        public Product()
+        {
+            Gallery = new String[10]
+        }
+
+        public Product(int id, string name, string price, int qty, string image, string desc, string[] gallery)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Qty = qty;
+            Image = image;
+            Desc = desc;
+            Gallery = gallery;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Quantity: " + Qty);
+            Console.WriteLine("Image: " + Image);
+            Console.WriteLine("Desc: " + Desc);
+            Console.WriteLine("Gallery: " + Gallery);
+        }
+
+        public Boolean isOutOfStock()
+        {
+            return Qty > 0;
+        }
+
+        public void AddToGallery(string image)
+        {
+            if (Gallery.Length == 10)
+            {
+                Console.WriteLine("Number of Images (10) reached. Please remove some to add another.");
+                return;
+            }
+
+            for (int i = 0; i < 10; ++i)
+            {
+                if (string.Equals(Gallery[i], ""))
+                {
+                    Gallery[i] = image;
+                    Console.WriteLine("Image successfully added.");
+                    return;
+                }
+            }
+        }
+        
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            try
+            {
+                Console.Write("n = ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            int f0 = 0;
+            int f1 = 1;
+            
+            for (;;)
+            {
+                if (f0 + f1 >= n) break;
+                f1 += f0;
+                f0 = f1 - f0;
+            }
+            
+            Console.WriteLine(f1);
+        }
+
+        public static bool CheckPrime(int n)
+        {
+            if (n < 2) return false;
+            if (n < 4) return true;
+            for (int i = 2; i <= n / 2; ++i)
+            {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
