42676ea538c10825d10d08b09c5969d7605c1de4
thinhnw <thinhnv0412@gmail.com>
2021-05-21 10:11:09 +0700

    Lab 3

---
+++ ConsoleApplication1/lab3/Cart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication1.lab3
+{
+    public class Cart
+    {
+        public int id;
+        public string customerName;
+        public double grandTotal;
+        public List<Product> productList;
+        public string city;
+        public string country;
+
+        public Cart()
+        {
+            productList = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            productList.Add(product);
+            grandTotal += product.Price;
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (productList.Remove(product))
+            {
+                grandTotal -= product.Price;
+            }
+        }
+
+        public double CalcGrandTotalWithFee()
+        {
+            if (city.Equals("Ha Noi") || city.Equals("HCM")) return grandTotal * 101 / 100;
+            if (country.Equals("Vietnam")) return grandTotal * 102 / 100;
+            return grandTotal * 105 / 100;
+        }
+        
+        
+    }
+}
